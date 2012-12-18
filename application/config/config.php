a3cab610149b59318dc198cbb02ad7f77d0f2fb0
Brian Sparacino <bsparacino@gmail.com>
2012-12-18 02:17:09 +0000

    added ion auth

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'q5[8QjjWg8CoUv^6OeX=W_h^yp)hZGxE';
 
 /*
 |--------------------------------------------------------------------------
